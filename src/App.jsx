20718e31405a99fb55a279af98f020a45678a69b
Jaime Pineda <jpin730@pm.me>
2025-09-06 23:23:04 -0600

    feat: :sparkles: implement game logic and render Tic Tac Toe board

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react'
+import Square from './components/Square'
+import { INITIAL_BOARD } from './consts/board'
+import { PLAYERS } from './consts/players'
+
 const App = () => {
-  return <h1>Hello world!</h1>
+  const [player, setPlayer] = useState(PLAYERS.X)
+  const [board, setBoard] = useState(INITIAL_BOARD)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = player
+    setBoard(newBoard)
+
+    const newPlayer = player === PLAYERS.X ? PLAYERS.O : PLAYERS.X
+    setPlayer(newPlayer)
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+
+      <section className="game">
+        {board.map((value, index) => (
+          <Square key={index} index={index} updateBoard={updateBoard}>
+            <span>{value}</span>
+          </Square>
+        ))}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={player === PLAYERS.X}>{PLAYERS.X}</Square>
+        <Square isSelected={player === PLAYERS.O}>{PLAYERS.O}</Square>
+      </section>
+    </main>
+  )
 }
 
 export default App
