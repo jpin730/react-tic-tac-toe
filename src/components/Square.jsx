8c1aaa87963a2bbbde0b32301527dcf6404164ff
Jaime Pineda <jpin730@pm.me>
2025-09-06 23:22:24 -0600

    feat: :sparkles: add Square component with PropTypes validation

---
+++ src/components/Square.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <button className={className} onClick={handleClick}>
+      {children}
+    </button>
+  )
+}
+
+Square.propTypes = {
+  children: PropTypes.node,
+  isSelected: PropTypes.bool,
+  updateBoard: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+}
+
+export default Square
