c8fedae77f57bae3507526aa31abf74e33df077c
Jaime Pineda <jpin730@pm.me>
2025-09-06 23:36:53 -0600

    feat: :sparkles: add winner checking logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,41 @@ import { useState } from 'react'
 import Square from './components/Square'
 import { INITIAL_BOARD } from './consts/board'
 import { PLAYERS } from './consts/players'
+import { WINNER_COMBOS } from './consts/winner-combos'
 
 const App = () => {
   const [player, setPlayer] = useState(PLAYERS.X)
   const [board, setBoard] = useState(INITIAL_BOARD)
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+  }
 
   const updateBoard = (index) => {
+    const isEmpty = board[index] === null
+    if (!isEmpty || winner) return
+
     const newBoard = [...board]
     newBoard[index] = player
     setBoard(newBoard)
 
     const newPlayer = player === PLAYERS.X ? PLAYERS.O : PLAYERS.X
     setPlayer(newPlayer)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
